2bb03a63cdd34ef9829999a7130e7446bc22c376
“Roils <roshall@yeah.net>
2020-01-05 22:09:47 +0800

    基本绘图功能实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,60 +1,31 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+let ctx
 Page({
   data: {
-    motto: '我的第一个小程序',
-    userInfo: {},
-    count:0,
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  count:function(){
-    this.setData({
-      count:this.count++
-    })
-  },
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    pen: {
+      linewidth: 15,
+      color: 'red'
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onLoad: function (options) {
+    ctx = wx.createCanvasContext('mycanvas')
+    ctx.setStrokeStyle(this.data.pen.color)
+    ctx.setLineWidth(this.data.pen.linewidth);
+    ctx.setLineCap('round');
+    ctx.lineJoin = 'round';
+    console.log(ctx)
+  },
+  touchStart: function (e) {
+    ctx.moveTo(e.touches[0].x, e.touches[0].y)
+    console.log(e.touches[0].x, e.touches[0].y)
+  },
+  touchMove: function (e) {
+    let x = e.touches[0].x
+    let y = e.touches[0].y
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.draw(true);
+    ctx.moveTo(x,y)
   }
 })
